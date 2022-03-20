098ed43061b296e8403b28839d73c291635fbf1a
elifdiril <elifdiril6@gmail.com>
2022-03-20 11:25:42 +0300

    react project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SmsProvider } from "./context/SmsContext";
+import SmsProviderTable from "./components/SmsProviderTable";
+import SmsProviderButton from "./components/SmsProviderButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SmsProvider>
+        <SmsProviderButton isUpdate={true} />
+        <SmsProviderButton />
+        <SmsProviderTable />
+      </SmsProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
